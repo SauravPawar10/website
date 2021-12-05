c945622426c45f92bf54f952a68fce56a4cefd6b
SauravPawar10 <pawarsaurav52@gmail.com>
2021-12-05 21:27:07 +0530

    saurav

--- src/App.js
+++ src/App.js
@@ -5,15 +5,25 @@ import AboutUsScreen from "./Components/AboutAcademy/AboutUsScreen";
 import { Fragment } from "react";
 import HomeBanner from "./Components/Home/HomeBanner";
 import HomeShortIntroduction from "./Components/Home/HomeShortIntroduction";
+import HomeCards from "./Components/Home/HomeCards";
+import AthleticStaffPage from "./Components/AthleticStaff/AthleticStaffPage";
+import HomePage from "./Components/Home/HomePage";
 
 function App() {
   return (
     <Router basename={"/"}>
-      <HomeBanner />
+      {/* <HomeBanner />
       <HomeShortIntroduction />
+      <HomeCards /> */}
       <Fragment>
         <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+        </Routes>
+        <Routes>
           <Route path="/about-us" element={<AboutUsScreen />}></Route>
+        </Routes>
+        <Routes>
+          <Route path="/athletics-staff" element={<AthleticStaffPage />}></Route>
         </Routes>
       </Fragment>
     </Router>
---
+++ src/Components/AthleticStaff/AthleticStaffPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AthleticStaffHeading from './AthleticStaffHeading'
+
+const AthleticStaffPage = () => {
+    return (
+        <div>
+            <AthleticStaffHeading />
+        </div>
+    )
+}
+
+export default AthleticStaffPage
